4bcebd46bb5386f8c7777badaedf2f7ef238ed90
Nabor <abnormal951@gmail.com>
2024-04-26 19:54:01 -0600

    ejercicio 046 con for y while, for no funciona, while ay se va

--- JavaEjercios/src/libroEjercicosJavaBucles/Ejercicio_046.java
+++ JavaEjercios/src/libroEjercicosJavaBucles/Ejercicio_046.java
@@ -1,15 +1,26 @@
-package libroEjercicosJava;
+package libroEjercicosJavaBucles;
 
 import java.util.Scanner;
 
-public class Ejercicio_46 {
+public class Ejercicio_046 {
 
 	public static void main(String[] args) {
+		
 		Scanner entrada = new Scanner(System.in);
-		System.out.println("");
-
-
+		String numero="";
+		while (!numero.equals("-1")) {
+			
+			System.out.println("ingresa un numero: para salir escribe -1");
+			numero=entrada.nextLine();
+			System.out.println("El numero que introdujiste tiene "+(numero.length())+" digitos");
+			
+			
+		} 
+		
 		entrada.close();
+		 {
+			
+		}
 	}
 
 }
--- JavaEjercios/src/libroEjercicosJavaBucles/Ejercicio_047.java
+++ JavaEjercios/src/libroEjercicosJavaBucles/Ejercicio_047.java
@@ -1,15 +1,30 @@
-package libroEjercicosJava;
+package libroEjercicosJavaBucles;
 
 import java.util.Scanner;
 
-public class Ejercicio_47 {
+public class Ejercicio_047 {
 
 	public static void main(String[] args) {
 		Scanner entrada = new Scanner(System.in);
-		System.out.println("");
 
+		System.out.println("Ingresa el numero a elevar");
+		int elevar=entrada.nextInt();
 
+		
+		for (int i = elevar; i <5000; i*=i) {
+			System.out.println(i);
+			
+		}
+	
+		int contador=0;
+		int i=0;
+		while(contador<5) {
+			System.out.println(" :"+i++);
+			contador++;
+		}
+		
 		entrada.close();
+
 	}
 
 }
