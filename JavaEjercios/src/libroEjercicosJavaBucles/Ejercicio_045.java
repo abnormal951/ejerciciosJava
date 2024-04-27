81423269cd5a83a075134a263d1ad67d25b40704
Nabor <abnormal951@gmail.com>
2024-04-26 18:12:51 -0600

    ejercicio 045

--- JavaEjercios/src/libroEjercicosJavaBucles/Ejercicio_045.java
+++ JavaEjercios/src/libroEjercicosJavaBucles/Ejercicio_045.java
@@ -1,15 +1,32 @@
-package libroEjercicosJava;
+package libroEjercicosJavaBucles;
 
 import java.util.Scanner;
 
-public class Ejercicio_45 {
+public class Ejercicio_045 {
 
 	public static void main(String[] args) {
 		Scanner entrada = new Scanner(System.in);
-		System.out.println("");
+		String convinacion;
+		String password="zxcv";
+		int contador=0;
 
+		do {
+			
+		
+		System.out.println("¿Cual es la contraseña?");
+		convinacion=entrada.nextLine();
+		if (password.equals(convinacion) ) {
+			System.out.println("Bienvenido");
+		} else {
+			System.out.println("esa no es la contraseña");
+			contador+=1;
+		} if (contador>=4) {
+			break;
+		}
+	} while (!password.equals(convinacion));
+
+		System.out.println("intentos: "+contador);
 
 		entrada.close();
 	}
-
 }
