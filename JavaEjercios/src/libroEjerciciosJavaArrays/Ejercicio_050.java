ff501cbddc3cf8d974553877592e16b7e8f5cd1f
Nabor <abnormal951@gmail.com>
2024-05-02 12:52:01 -0600

    ejercios for

--- JavaEjercios/src/libroEjerciciosJavaArrays/Ejercicio_050.java
+++ JavaEjercios/src/libroEjerciciosJavaArrays/Ejercicio_050.java
@@ -1,15 +1,45 @@
 package libroEjerciciosJavaArrays;
 
-import java.util.Scanner;
-
 public class Ejercicio_050 {
 
 	public static void main(String[] args) {
-		Scanner entrada = new Scanner(System.in);
-		System.out.println("");
+		int []num = new int[12];
+
+		num[0] =39;
+		num[1] =-2;
+		num[2] =0;
+		num[3] =0;
+		num[4] =0;
+		num[5] =0;
+		num[6] =14;
+		num[7] =0;
+		num[8] =5;
+		num[9] =120;
+		num[10] =0;
+		num[11] =0;
+
+		for(int i = 0; i < 12; i++){
+			System.out.println(num[i]);
+		}
+
+		String []simbolo = new String[10];
+
+		simbolo[0] ="'a'";
+		simbolo[1] ="'x'";
+		simbolo[2] ="'0'";
+		simbolo[3] ="'0'";
+		simbolo[4] ="'@'";
+		simbolo[5] ="'0'";
+		simbolo[6] ="' '";
+		simbolo[7] ="'+'";
+		simbolo[8] ="'Q'";
+		simbolo[9] ="'0'";
+		
+		for(int i =0;i<=9;i++) {
+			System.out.println(simbolo[i]);
+		}
 
 
-		entrada.close();
 	}
 
 }
--- JavaEjercios/src/libroEjerciciosJavaArrays/Ejercicio_051.java
+++ JavaEjercios/src/libroEjerciciosJavaArrays/Ejercicio_051.java
@@ -5,11 +5,21 @@ import java.util.Scanner;
 public class Ejercicio_051 {
 
 	public static void main(String[] args) {
-		Scanner entrada = new Scanner(System.in);
-		System.out.println("");
+		Scanner entrada = new Scanner(System.in);//instancia de Scanner para la entrada de datos
+		int []numeros = new int[5];// declarar el array de 10 posiciones
+		
+		
+		System.out.println("ingresa 10 numeros");
 
+		for (int i = 0; i < 5; i++) {
+			System.out.println("Numero : "+(i+1)+ " :");
+			numeros[i] = entrada.nextInt();
 
+		}
+		for (int i =4; i >= 0; i--) {
+			System.out.println("for "+numeros[i]);
+		}
+	
 		entrada.close();
 	}
-
 }
