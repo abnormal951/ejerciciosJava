c5a0982156639aeb9ab3c65609d3360b6745a9c5
Nabor <abnormal951@gmail.com>
2024-05-03 00:56:14 -0600

    se agregan 2 ejercios deitel

--- JavaEjercios/src/deitelEjercicios/Ejercicio_002.java
+++ JavaEjercios/src/deitelEjercicios/Ejercicio_002.java
@@ -1,11 +1,33 @@
 package deitelEjercicios;
 
+import java.util.Scanner;
+
 public class Ejercicio_002 {
 
 	public static void main(String[] args) {
+	Scanner entrada = new Scanner(System.in);
+
+		System.out.println("BMI");
+		System.out.println("ingresa tu peso en Kg");
+		float peso = entrada.nextFloat();
+		System.out.println("ingresa tu altura en metros");
+		float altura = entrada.nextFloat();
+
+		float BMI=peso/(altura*altura);
+
+
+
+		if (BMI <= 18.5) {
+			System.out.println("Bajp de peso: "+BMI);
+		} else if (BMI > 18.5 && BMI < 24.9){
+			System.out.println("normal: "+BMI);
+		} else if (BMI > 25 && BMI < 29.9){
+			System.out.println("sobrepeso: "+BMI);
+		} else{
+			System.out.println("obeso: "+BMI);
+		}
 		
-
-
+		entrada.close();
 	}
 
 }
